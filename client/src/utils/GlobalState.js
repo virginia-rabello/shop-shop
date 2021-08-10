ee3742219a4ee2a161cec8f0aa9f441df42c8aa2
Virginia Rabello <virginia.rabello2107@gmail.com>
2021-08-10 15:46:59 -0500

    Cart Functionality

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,9 +5,11 @@ const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: ''
     });
     // use this to confirm it works!
     console.log(state);
